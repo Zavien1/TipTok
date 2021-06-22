f5be24e9b1eb9470236e0898e4720020e5bdbd38
Zavien Sibilia <zavien@fb.com>
2021-06-22 15:45:01 -0700

    create settings page

---
+++ TipTok/SettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  TipTok
+//
+//  Created by Zavien Sibilia on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
